d718c8066f298965d03e1285d8d961e7dae8512f
jpcamatari <jpcamatari@hotmail.com>
2023-10-10 00:19:23 -0300

    Static files upload

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastro/', views.cadastro, name='cadatro'),
+    path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.logar, name="logar"),
 ]
